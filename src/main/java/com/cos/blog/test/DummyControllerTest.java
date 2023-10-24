86db42c14811559af82046be85f5362d5b291ac5
seedit1 <woom1441@naver.com>
2023-10-24 15:14:37 +0900

    업데í이트

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -11,6 +11,8 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.blog.model.RoleType;
@@ -27,6 +29,21 @@ public class DummyControllerTest {
 	@Autowired //의존성 주입(DI)
 	private UserRepository userRepository;
 	
+	@PutMapping("/dummy/user/{id}")
+	public User updateUser(@PathVariable int id,@RequestBody User requestUser) { //json데이터를 요청 => Java Object(MessageConverter의 
+		System.out.println("id :" + id);                                                                                            //Jackson라이브러리가 변환해서 받아준다.
+		System.out.println("password :" + requestUser.getPassword());
+		System.out.println("email :" + requestUser.getEmail());
+		
+		User user = userRepository.findById(id).orElseThrow(() -> {
+			return new IllegalArgumentException("수정에 실패하였습니다.");
+		});
+		user.setPassword(requestUser.getPassword());
+		user.setEmail(requestUser.getEmail());		
+		userRepository.save(user);
+		return null;
+	}
+	
 	//http://localhost:8000/blog/dummy/user
 	@GetMapping("/dummy/users")
 	public List<User> list(){
@@ -36,8 +53,10 @@ public class DummyControllerTest {
 	
 	//한페이지당 2건의 데이터를 리턴받아 볼 예정
 	@GetMapping("/dummy/user")
-	public  Page<User> pageList(@PageableDefault(size=2, sort="id", direction = Sort.Direction.DESC) Pageable pageable){
-		Page<User> users = userRepository. findAll(pageable);
+	public List<User> pageList(@PageableDefault(size=2, sort="id", direction = Sort.Direction.DESC) Pageable pageable){
+		Page<User> pagingUser = userRepository. findAll(pageable);
+		
+		List<User> users = pagingUser.getContent();
 		return users;
 	}
 	//{id}주소를 파라미터로 전달 받을 수 있다.
